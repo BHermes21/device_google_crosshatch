cb4b6dc5645e29eda38927947a4fc7c65c9eda37
nysascape <jago@nysascape.digital>
2020-05-06 13:27:07 +0000

    Crosshatch: Add some missing flags for device
    * Thanks to https://github.com/LineageOS/android_device_google_crosshatch/blob/lineage-17.1/device-lineage.mk
    
    Change-Id: I9d50bb47454f8cc8aad2e70ee3630e9c3ce80a0d
    Signed-off-by: nysascape <jago@nysascape.digital>

--- device.mk
+++ device.mk
@@ -865,4 +865,19 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.opa.eligible_device=true \
     ro.gfx.angle.supported=true
 
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
+# EUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml
+
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true \
+    media.mediadrmservice.enable=true
+
 PRODUCT_VENDOR_KERNEL_HEADERS := device/google/crosshatch/sdm845/kernel-headers
